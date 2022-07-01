48c426446a855d850b31861cf371e2d2740dd6d6
qazs10015 <happystyle666@gmail.com>
2022-07-01 20:36:18 +0800

    修改環境變數

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
-  redirectUri: 'https://qazs10015.github.io/OAuthLoginByLINE',
+  redirectUri: 'https://oauthloginbyline.netlify.app',
+  // redirectUri: 'https://qazs10015.github.io/OAuthLoginByLINE',
   clientId_LINE_Login: '1657255693',
   clientId_LINE_Notify: 'WwVpq6CPRKeAAv36AeW2HW'
 };
